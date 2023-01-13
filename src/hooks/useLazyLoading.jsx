765d68edbd03c8b389a6fec3f0d8f2cc07420cd7
hyperstone1 <kingofthegorgon@mail.ru>
2023-01-13 04:51:01 +0300

    fixed warnings

--- src/hooks/useLazyLoading.jsx
+++ src/hooks/useLazyLoading.jsx
@@ -1,33 +1,21 @@
-import { useRef, useCallback } from "react";
-import throttle from "lodash/throttle";
+import { useRef, useCallback } from 'react';
+import throttle from 'lodash/throttle';
 
-export function useLazyLoading({
-  onIntersection,
-  delay = 1000,
-  marginFromBottom = 10
-}) {
+export function useLazyLoading({ onIntersection, delay = 1000, marginFromBottom = 10 }) {
   const containerRef = useRef(null);
 
-  // Функция обработчик scroll эвента, с ограничением количества вызовов
-  // посредством lodash/throttle
+  // eslint-disable-next-line
   const onScroll = useCallback(
     throttle(() => {
       const containerScrollTop = containerRef.current.scrollTop;
       const containerHeight = containerRef.current.clientHeight;
       const scrollHeight = containerRef.current.scrollHeight;
 
-      if (
-        scrollHeight -
-          containerScrollTop -
-          containerHeight -
-          marginFromBottom <=
-        0
-      ) {
+      if (scrollHeight - containerScrollTop - containerHeight - marginFromBottom <= 0) {
         onIntersection();
       }
     }, delay),
-     // eslint-disable-next-line
-    [onIntersection, containerRef, marginFromBottom, delay]
+    [onIntersection, containerRef, marginFromBottom, delay],
   );
 
   return [onScroll, containerRef];
